4c5899ef2f9107678e2a3f61de388dc7d08d9302
Hithakshi <hithakshikulal@gmail.com>
2021-10-21 16:15:52 +0530

    Add travis CI comfig

--- EssentialFeed/EssentialFeed/EssentialFeed.h
+++ EssentialFeed/EssentialFeed/EssentialFeed.h
@@ -16,3 +16,4 @@ FOUNDATION_EXPORT const unsigned char EssentialFeedVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <EssentialFeed/PublicHeader.h>
 
 
+
